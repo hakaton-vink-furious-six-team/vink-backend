11d5f4c4cf6b0d57d652f03180372c3209248fa6
Vladimir Shevchenko <prstud@yandex.ru>
2024-03-27 20:26:03 +0300

    add daphne app to settings.py

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
