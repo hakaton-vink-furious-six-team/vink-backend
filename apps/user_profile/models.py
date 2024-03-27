036b735837fa4c3cae7e4426bb6e82544a8c731f
Vladimir Shevchenko <prstud@yandex.ru>
2024-03-27 14:52:48 +0300

    fix mistake in length of phone_number field in user_profile model

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -11,7 +11,7 @@ class UserProfile(models.Model):
         message="Номер телефона должен быть в формате:"
         " '+999999999'. Допускается до 15 цифр.",
     )
-    phone_number = models.CharField(validators=[phone_regex], max_length=17)
+    phone_number = models.CharField(validators=[phone_regex], max_length=15)
     company_name = models.CharField(max_length=100, blank=True, null=True)
     date_added = models.DateTimeField(auto_now=True)
 
