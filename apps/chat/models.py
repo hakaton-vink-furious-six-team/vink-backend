03594d7c44bd48127d5f552b92f6ce73771efeab
Vladimir Shevchenko <prstud@yandex.ru>
2024-03-27 17:22:53 +0300

    created chat model

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,27 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from apps.user_profile.models import UserProfile
+from apps.message.models import Message
+
+
+class Chat(models.Model):
+    """Модель чата."""
+
+    STATUS_CHOICES = (
+        ("open", "Открыт"),
+        ("closed", "Закрыт"),
+    )
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="chats"
+    )
+    messages = models.ForeignKey(
+        Message, on_delete=models.CASCADE, related_name="chat_messages"
+    )
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default="open"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Chat {self.id}"
