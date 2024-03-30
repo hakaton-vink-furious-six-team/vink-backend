5bba9d8593cb3277d5db4cdcbbb4fe2380ee676e
Vladimir Shevchenko <prstud@yandex.ru>
2024-03-30 21:20:42 +0300

    add restframework lib, userprofileserializer, userprofileviewset, urls paths

--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -1,13 +1,11 @@
 from rest_framework import viewsets
 
 from apps.user_profile.api_v1.serializers import UserProfileSerializer
-from apps.user_profile.models import UserProfile
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Отправка данных о пользователе."""
 
-    queryset = UserProfile.objects.all()  # noqa
     serializer_class = UserProfileSerializer
     http_method_names = [
         "post",
