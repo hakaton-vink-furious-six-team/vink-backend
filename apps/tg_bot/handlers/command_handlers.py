3046eb6522f54f6c6bc28b3c44cc0c45307f3ef4
Vladimir Shevchenko <prstud@yandex.ru>
2024-04-01 14:06:10 +0300

    add user verification at the start

--- apps/tg_bot/handlers/command_handlers.py
+++ apps/tg_bot/handlers/command_handlers.py
@@ -5,6 +5,7 @@ from telebot.types import Message, CallbackQuery  # noqa
 
 from apps.tg_bot.message_templates.base_messages import BaseMessages
 from apps.tg_bot.states.profile_states import ProfileStateGroup
+from apps.tg_bot.utils.utils import check_user_exists
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 def start_process(message: Message, bot: TeleBot):
     """Обработка команды старт."""
 
-    if message.from_user.id not in []:
+    if not check_user_exists(message.from_user.id):
         bot.set_state(
             message.from_user.id, ProfileStateGroup.get_name, message.chat.id
         )
