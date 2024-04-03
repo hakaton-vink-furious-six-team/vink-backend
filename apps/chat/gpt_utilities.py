ba6b256acee807919850f444a1df1c32c6036d30
Тимофей Карпов <acuna-think@yandex.ru>
2024-04-03 13:33:11 +0300

    add sync_gpt_answer without database using

--- apps/chat/gpt_utilities.py
+++ apps/chat/gpt_utilities.py
@@ -1,10 +1,18 @@
 from aiohttp import ClientSession
 from django.http import HttpResponse
 from .models import ProjectSettings
+import requests
+
 
 SORRY_TEXT = (
     'В настоящий момент все операторы заняты, обратитесь пожалуйста позднее.'
 )
+YGPT_API_KEY = "AQVN3w8RT5FZ9zjc1clPFoV6hhJTAo76pg4FyIUv"
+CATALOG_ID = "b1gqr96555efj5p78okn"
+PRO = "/yandexgpt/latest"
+LITE = "/yandexgpt-lite/latest"
+MODEL_URI = "gpt://" + CATALOG_ID + LITE
+URL_COMPL = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
 
 
 async def get_gpt_answer(message):
@@ -67,3 +75,33 @@ async def ask_run(args):
     answer = await get_gpt_answer(begin_conversation)
     return HttpResponse(answer)  # для отладки
     # а на проде тут надо возвращать веб-сокетам
+
+
+def sync_gpt_answer(messages):
+    prompt = {
+        "modelUri": MODEL_URI,
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.5,
+            "maxTokens": 500
+        },
+        "messages": [
+            {
+                "role": "system",
+                "text": "Ты онлайн консультант в чате поддержки пользователей."
+            },
+        ]
+    }
+    prompt["messages"].extend(messages)
+    auth = {
+        "Content-Type": "application/json",
+        "Authorization": "Api-Key " + YGPT_API_KEY
+    }
+    response = requests.post(URL_COMPL, headers=auth, json=prompt)
+    js_res = response.json()
+    result = js_res.get('result')
+    if result:
+        answer = result['alternatives'][0]['message']['text']
+    else:
+        answer = SORRY_TEXT
+    return answer
