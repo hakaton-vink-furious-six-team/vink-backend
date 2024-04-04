e2b4643113102012a92924842bbcaa40614d3959
Тимофей Карпов <acuna-think@yandex.ru>
2024-04-04 17:27:34 +0300

    add docker-compose setup to faster deploy

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+python manage.py makemigrations
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+cp -r /app/collected_static/. /backend/static
+
+# daphne config.wsgi:application
+gunicorn config.wsgi --bind 0.0.0.0:8000
