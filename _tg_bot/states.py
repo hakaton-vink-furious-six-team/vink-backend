80ea431967a72fcdff033ad5c7ae238e26eea546
Vladimir Shevchenko <prstud@yandex.ru>
2024-04-04 21:24:41 +0300

    .

--- _tg_bot/states.py
+++
@@ -1,5 +0,0 @@
-from telebot.handler_backends import StatesGroup, State
-
-
-class AssessmentsStateGroup(StatesGroup):
-    get_assessment = State()
--- _tg_bot/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path
-from src.apps.tg_bot import views
-
-urlpatterns = [path("", views.bot_view)]
