34415c1669bb96d2fef0c46cf70f23e9e4557ec5
Vladimir Shevchenko <prstud@yandex.ru>
2024-04-05 22:18:00 +0300

    add local settings to dockerignore, delete gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ python manage.py collectstatic --no-input
 
 cp -r /app/collected_static/. /backend/static
 
-# daphne config.wsgi:application
-gunicorn config.wsgi --bind 0.0.0.0:8000
+daphne config.asgi:application
