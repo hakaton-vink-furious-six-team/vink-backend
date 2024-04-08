17c631645c185ee6b6bb1ca1ec9fccc6149559e8
Тимофей Карпов <acuna-think@yandex.ru>
2024-04-08 16:41:32 +0300

    fix for ssl conections

--- config/asgi.py
+++ config/asgi.py
@@ -9,7 +9,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django_asgi_app = get_asgi_application()
 
 from apps.chat.routing import websocket_urlpatterns  # noqa
+from apps.tg_bot.main import bot_starter  # noqa
 
+bot_starter()
 
 application = ProtocolTypeRouter(
     {
